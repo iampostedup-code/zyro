068407efbf80aecb63518461b54f7ca5d21bd99f
Zyros Scripts <iampostedup@gmail.com>
2026-02-20 20:31:09 +1000

    Load external script from URL in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
--- The main loader script (key system, or game checker for example) go here (obfuscated!)
+loadstring(game:HttpGet("https://gist.githubusercontent.com/iampostedup-code/f7803b28054a5ec95a2237f5c98f0799/raw/71c1accadde5074f02007f1319961ddab1643904/ZyrosAimLock"))()
